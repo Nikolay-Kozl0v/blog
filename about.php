cef279f5e50f41fd77f7e24813f150ca3982325a
Nikolay-Kozl0v <165823058+Nikolay-Kozl0v@users.noreply.github.com>
2024-04-17 07:11:00 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,45 @@
+<?php
+include ("path.php");
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+          integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <!-- Custom Styling -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <title>О сайте</title>
+</head>
+<body>
+
+<?php include("app/include/header.php"); ?>
+
+<div class="container">
+    <div class="content row">
+        <!-- Main Content -->
+        <div class="main-content col-md-9 col-12">
+            <h2>О сайте</h2>
+            <h4>Добро пожаловать на сайт MyBlog!<br>
+
+                MyBlog - это курсовая работа, созданная с целью изучения и применения навыков веб-разработки.<br> Этот проект нацелен на создание персональной п
+                латформы для ведения личного блога, где вы можете делиться своими мыслями,
+                идеями и опытом с сообществом.<br>Я стремился к созданию уютного уголка в интернете, где вы можете выражать себя и общаться с единомышленниками.<br>
+                <br>
+                Спасибо, что посетили MyBlog!</h4>
+
+            </div>
+
+        </div>
+
+<?php include("app/include/footer.php"); ?>
